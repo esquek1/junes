e7b6deafbfcf070dca8af92406a6f7a43f1c82f6
esquek1 <esquejokelly@gmail.com>
2024-11-13 21:56:39 -0800

    Add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
 import React, { useState } from "react";
 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar.jsx";
+import Home from "./components/Home.jsx";
+import Menu from "./components/Menu.jsx";
 import Contact from "./components/Contact.jsx";
+
 function App() {
     return (
         <div className="App">
             <Router>
                 <Navbar />
                 <Routes>
-                    <Route exact path="/" element={<h1>Home Page</h1>} />
-                    <Route exact path="contact" element={<Contact />} />
+                    <Route index element={<Home />} />
+                    <Route path="menu" element={<Menu />} />
+                    <Route path="contact" element={<Contact />} />
                 </Routes>
+                <div className="list">
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="menu">Menu</Link>
+                        </li>
+                        <li>
+                            <Link to="contact">Contact</Link>
+                        </li>
+                    </ul>
+                </div>
             </Router>
         </div>
     );
